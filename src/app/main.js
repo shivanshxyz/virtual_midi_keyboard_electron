832324de8b00bf0d9742806a18fd3a57477a2c96
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-13 01:40:13 +0530

    updated src/app/main.js

--- src/app/main.js
+++ src/app/main.js
@@ -9,3 +9,19 @@ import SocketServer from "./server/SocketServer";
 import * as actionCreators from "./actions";
 import * as types from "../common/ActionTypes";
 import { DEVICE_NAME } from "../common/constants";
+
+const PUBLIC_PATH = path.join(__dirname, "..", "..", "public");
+
+const store = createStore(reducers, applyMiddleware(inject(midiHandler)));
+const actions = bindActionCreators(actionCreators, store.dispatch);
+const midiDevice = new MIDIDevice(DEVICE_NAME, actions);
+
+let mainWindow = null;
+let server = null;
+let state = store.getState();
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
