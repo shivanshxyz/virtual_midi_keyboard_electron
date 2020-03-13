b5c7e04dd6cdcf9b2f7432291b4a74a4d2261e5b
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-13 17:24:03 +0530

    updates

---
+++ test/client/reducers/index.js
@@ -0,0 +1,34 @@
+import "run-with-mocha";
+import assert from "assert";
+import reducer from "../../../src/client/reducers";
+import * as types from "../../../src/common/ActionTypes";
+
+describe("reducer", () => {
+  it("init state", () => {
+    const initState = reducer(undefined, {});
+
+    assert(typeof initState === "object");
+  });
+
+  it("SET_STATE", () => {
+    const state = { foo: 0, bar: 1 };
+    const action = { type: types.SET_STATE, state: { foo: 1, baz: 3 } };
+    const nextState = reducer(state, action);
+    const expected = { foo: 1, bar: 1, baz: 3 };
+
+    assert(state !== nextState);
+    assert.deepEqual(nextState, expected);
+  });
+
+  it("APPLY_PATCH", () => {
+    const state = { foo: 0, bar: 1 };
+    const action = { type: types.APPLY_PATCH, patch: [
+       { op: "replace", path: "/bar", value: 2 },
+    ] };
+    const nextState = reducer(state, action);
+    const expected = { foo: 0, bar: 2 };
+
+    assert(state !== nextState);
+    assert.deepEqual(nextState, expected);
+  });
+});
